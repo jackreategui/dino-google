33405221564b9f52471948d7bbc9472e1084afad
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-04-14 17:46:02 -0500

    arregle el error que tenia por no llamar de forma correcta a una case

---
+++ main.js
@@ -0,0 +1,65 @@
+let time = new Date();
+let deltaTime = 0;
+
+if (document.readyState === "complete" || document.readyState === "interactive") {
+    setTimeout(Init, 1);
+} else {
+    document.addEventListener("DOMContentLoaded", Init);
+}
+
+function Init() {
+    time = new Date();
+    Start();
+    Loop();
+}
+
+function Loop() {
+    deltaTime = (new Date() - time) / 1000;
+    time = new Date();
+    Update();
+    requestAnimationFrame(Loop);
+}
+
+let sueloY = 22;
+let velY = 0;
+let impulso = 900;
+let gravedad = 2500;
+
+let dinoPosX = 42;
+let dinoPosY = sueloY;
+
+let sueloX = 0;
+let velEscenario = 1280 / 3;
+let gameVel = 1;
+let score = 0;
+
+let parado = false;
+let saltando = false;
+
+let contenedor;
+let dino;
+let textoScore;
+let suelo;
+let gameOver;
+
+function Start() {
+    gameOver = document.querySelector(".game-over");
+    suelo = document.querySelector(".suelo");
+    contenedor = document.querySelector(".contenedor");
+    textoScore = document.querySelector(".score");
+    dino = document.querySelector(".dino");
+}
+
+function Update() {
+    MoverSuelo();
+}
+
+function MoverSuelo() { 
+    sueloX += CalcularDesplazamiento();
+    suelo.style.left = -(sueloX % contenedor.clientWidth) + 'px';
+}
+
+function CalcularDesplazamiento() { 
+    return velEscenario * deltaTime * gameVel;
+
+}
